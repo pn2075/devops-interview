02c953b52f00020120d01b501ac0f198a3cfd9bc
Han <pn2075@g.rit.edu>
2024-01-17 03:22:35 -0500

    All the stuff

---
+++ lambda_function/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lambda_function',
+    version='1.0',
+    packages=['lambda_function'],
+    install_requires=[
+        # dependencies 
+    ],
+)
